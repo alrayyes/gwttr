8b3bf6bf78a19c95822ac3707e8123c59d8a422d
Ryan Kes <ryan@ryankes.eu>
2025-02-18 19:36:20 +0100

    refactor: inject context into request

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -18,7 +19,12 @@ func main() {
 		Timeout:       timeout,
 	}
 
-	req, err := http.NewRequest(http.MethodGet, "https://wttr.in/honolulu?0A", nil)
+	req, err := http.NewRequestWithContext(
+		context.Background(),
+		http.MethodGet,
+		"https://wttr.in/honolulu?0A",
+		nil,
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
